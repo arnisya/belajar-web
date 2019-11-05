6475b83dd4be7df28f0c897c5daa47f3884ed4b6
arnisya <arnisyaichabahri@gmail.com>
2019-11-05 21:41:46 +0800

    file project web

---
+++ python1.py
@@ -0,0 +1,6 @@
+counter = 1
+while True:
+    print("here's Irham!")
+    counter += 1
+    if counter > 5:
+        break
---
+++ python2.py
@@ -0,0 +1,9 @@
+lannister = "JaimeCerseiTyrion"
+
+a = lannister [1]
+b = lannister [0]
+c = lannister [0:5]
+d = lannister [5:11]
+e = lannister [11:17]
+
+print(a, b, c, d, e)
